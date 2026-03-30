cc9a7ecf8887f22f96f897c89f0b6f6d1b4a2ccb
Jasmine <jasmine.cole@crpusd.org>
2026-03-30 09:20:18 -0700

    horse data step PLTW

--- HorseData.java
+++ HorseData.java
@@ -10,5 +10,35 @@ public class HorseData
     public static void main(String[] args) throws IOException
     {
         Scanner sc = new Scanner(new File("horsedata.txt"));
+
+        String name;
+        int weight;
+        double age;
+        boolean newInBarn;
+        int sumWeight = 0;
+        double sumAge = 0.0;
+        int i;
+        double totalHay = 0.0;
+        for (i = 0; sc.hasNextLine(); i++)
+        {
+            name = sc.next();
+            weight = sc.nextInt();
+            age = sc.nextDouble();
+            newInBarn = sc.nextBoolean();
+            sumWeight += weight;
+            sumAge += age;
+            totalHay += (weight * 2.5);
+            if (newInBarn == true)
+            {
+                System.out.println("(NEW) " + name + " weighs " + weight + " at " + age + " years old.");
+            }
+            else
+            {
+                System.out.println(name + " weighs " + weight + " at " + age + " years old.");
+            }  
+        }
+        System.out.println("Average weight: " + sumWeight/i);
+        System.out.println("Average age: " + sumAge/i);
+        System.out.println("Total hay needed: " + totalHay);
     }
 }
