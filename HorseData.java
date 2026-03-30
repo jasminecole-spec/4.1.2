484deedbed33f2b4d5d8a7f85d73762a7cd1a5ed
jasminecole-spec <jasmine.cole@crpusd.org>
2026-03-30 08:41:23 -0700

    Add files via upload

---
+++ HorseData.java
@@ -0,0 +1,14 @@
+/* 
+ * Activity 4.1.2
+ */
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class HorseData
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner sc = new Scanner(new File("horsedata.txt"));
+    }
+}
