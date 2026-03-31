c4a9ab08947a6e5a874d3040e00a3a7e6bcbd3d5
Jasmine <jasmine.cole@crpusd.org>
2026-03-31 08:48:44 -0700

    up to step 22

--- PracticeSchedule.java
+++ PracticeSchedule.java
@@ -9,17 +9,25 @@ public class PracticeSchedule
 {
     public static void main(String[] args) throws IOException
     {
-      Scanner sc1 = new Scanner(new File("schedule1.csv"));
+      Scanner sc1 = new Scanner(new File("schedule2.csv"));
       
       String str;
       String[] tokens;
+      double hours = 0;
+      double totalHours = 0;
+      int index = 0;
       
       sc1.nextLine();
       while (sc1.hasNext()) {
+        index++;
         str = sc1.nextLine();
         tokens =  str.split(",");
-        System.out.println(tokens[0] + " I practice " + tokens[1]  + " hours.");
+        hours = Double.parseDouble(tokens[1]);
+        totalHours += hours;
+        System.out.println(tokens[0] + " I practice " + hours  + " hours.");
       }
+      System.out.println("Total amount of practice per week: " + totalHours);
+      System.out.println("Average amount of practice per day: " + totalHours/index);
       
       sc1.close();
         
